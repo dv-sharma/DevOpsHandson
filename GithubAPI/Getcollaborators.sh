5eea4eda6918c8ba7e4a85894f6169e247dded43
dv-sharma <divyam.sharma3@gmail.com>
2023-12-06 19:53:37 -0500

    script edited to get collaborators

--- GithubAPI/Getcollaborators.sh
+++ GithubAPI/Getcollaborators.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ############################
-Author : Divyam Sharma
-This script will help users to retrieve information from Github
-Usage: Please provide your github token and rest api to script as input
+#Author : Divyam Sharma
+#This script will help users to retrieve information from Github
+#Usage: Please provide your github token and rest api to script as input
 
 #############################
 
@@ -13,8 +13,8 @@ if [ ${#@} -lt 2 ]; then
 fi
 API_URL="https://api.github.com"
 
-USERNAME= $username
-TOKEN= $token
+USERNAME=$username
+TOKEN=$token
 GITHUB_OWNER=$1
 GITHUB_REPO_NAME=$2
 
@@ -22,17 +22,17 @@ GITHUB_REPO_NAME=$2
 #### Creating a function to make the REST API hit
 
 
-GITHUB_API_URL=   "https://api.github.com//repos/GITHUB_OWNER/GITHUB_REPO_NAME/collaborators"
+GITHUB_API_URL=   "https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPO_NAME}/collaborators"
 
 response= $(curl -s -H "Authorization: token ${TOKEN}" "${GITHUB_API_URL}")
 
-if [$? -ne 0]; then
+if [ $? -ne 0 ]; then
     echo "Error: Unable to fetch collaborators. Please check your GitHub token and repository name."
     exit 1
 fi
 collaborators=$(echo "${response}" | jq -r '.[].login')
 
-echo "Collaborators for ${REPO_NAME}:"
+echo "Collaborators for ${GITHUB_REPO_NAME}:"
 echo "${collaborators}"
 
 
